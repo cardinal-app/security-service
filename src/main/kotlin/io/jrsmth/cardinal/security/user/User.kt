b1cf5ffb716944813d921fae2410bf0b0f1ec880
jrsmth-tier2 <jsmith@tier2consulting.com>
2024-04-03 12:27:06 +0100

    [CARD-69] Update schema with user model

--- src/main/kotlin/io/jrsmth/cardinal/security/user/User.kt
+++ src/main/kotlin/io/jrsmth/cardinal/security/user/User.kt
@@ -1,17 +1,19 @@
 package io.jrsmth.cardinal.security.user
 
 import io.jrsmth.cardinal.common.model.AbstractEntity
+import jakarta.persistence.Entity
 import jakarta.persistence.Table
 
 /**
  * Cardinal User
  * @since 26th March 2024
  */
-@Table(name = "user")
+@Entity
+@Table(name = "app_user")
 data class User (
-    var active: Boolean,
-    var firstName: String?,
-    var lastName: String?,
-    var email: String?,
-    var password: String?,
+    var active: Boolean? = null,
+    var firstName: String? = null,
+    var lastName: String? = null,
+    var email: String? = null,
+    var password: String? = null
 ) : AbstractEntity()
